58ab3ff60cc3d240d559410eddcdadd3b5bbf955
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-22 13:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateOutput() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+updateOutput().then(text => {
+  document.getElementById("output").textContent = text;
+});
